949fb7d02cd465289e086d9a1844032fee4cba53
Padliwinata <rpadliwinata@gmail.com>
2024-06-29 17:14:11 +0700

    update

--- pages/enroll.py
+++ pages/enroll.py
@@ -7,9 +7,11 @@ st.title('Enroll Student')
 res = db_course.fetch()
 courses = [item['course'] for item in res.items]
 
-course = st.selectbox('Course', courses)
-student_id = st.text_area(label='Student ID')
+with st.form('enroll_student', clear_on_submit=True):
+    course = st.selectbox('Course', courses)
+    student_id = st.text_area(label='Student ID')
 
-for student in student_id.split('\n'):
-    db_student.put({'student_id': student, 'course': course})
+    for student in student_id.split('\n'):
+        db_student.put({'student_id': student, 'course': course})
 
+    st.form_submit_button('Submit')
